08e6506dba0ad5867e4472221a400026107d0e47
ela-mu2 <elias.pj.dev@gmail.com>
2026-09-01 11:44:21 +0800

    e

--- lessons/mvc/advanced-mongoose/index.js
+++ lessons/mvc/advanced-mongoose/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Blog = require("./models/Blog");
+const Comment = require('./models/Comment')
 
 mongoose
     .connect("mongodb://localhost:27017/advanced-mongoose")
@@ -10,6 +12,37 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
+
+app.get("/", async (req, res) => {
+    const blogs = await Blog.find({});
+    res.json(blogs);
+});
+
+app.get("/comment/:commentId", async (req, res) => {
+    const commentId = req.params.id
+    const comment = await Blog.find({ "comments._id": commentId })
+    res.json(comment)
+})
+
+app.post("/addBlogWithoutComments", async (req, res) => {
+    const blogWithoutComments = new Blog({
+        title: "New Blog without Comments",
+        author: "Josh Doe",
+        content: "Some new blog without comments inside",
+    });
+    await blogWithoutComments.save();
+    res.status(201).json(blogWithoutComments);
+});
+app.post("/addComment", async (req, res) => {
+    const newComment = new Blog({
+        user: "Adam",
+        content: "A comment with ObjectID Linking",
+        votes: 5000,
+        blog_id: "6a9648e4e4703ee31417e398"
+    });
+    await newComment.save();
+    res.status(201).json(newComment);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- lessons/mvc/advanced-mongoose/models/Blog.js
+++ lessons/mvc/advanced-mongoose/models/Blog.js
@@ -1,17 +1,10 @@
 const { Schema, model } = require("mongoose");
 
-const CommentSchema = new Schema({
-    user: String,
-    content: String,
-    votes: Number,
-});
-
 const BlogSchema = new Schema({
     title: String,
     author: String,
     content: String,
-    comments: [CommentSchema],
 });
 
 const Blog = model("Blog", BlogSchema);
-const Comment = model("Comment", CommentSchema);
+module.exports = Blog;
